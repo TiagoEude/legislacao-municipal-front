b5ad524c6292fd51973fca6ebcae0f6234c48b0f
Tiago Chaves <tiago@nca.ufma.br>
2021-07-17 20:53:00 -0300

    Conclusão da lista de publicações

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import Layout from "../components/Layout";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-config";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+class LegislacaoApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <ApolloProvider client={client}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ApolloProvider>
+      </>
+    );
+  }
 }
 
-export default MyApp
+export default LegislacaoApp;
